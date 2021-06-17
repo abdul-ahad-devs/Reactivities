ab3bd4c1365ea58450786e601535cf59492ec39a
abdul ahad <abdul.ahad@intelligenes.com>
2021-06-17 17:43:36 +0500

    mapped data from DB and enabled CORS policy

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+import './App.css';
 function App() {
+
+  const [activities, setActivities] = useState([]);
+
+  const fetchActivities = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/activities');
+
+      setActivities(response.data)
+    }
+    catch (exception) {
+      console.warn('Error fetching data', exception)
+
+    }
+  }
+
+  useEffect(() => {
+    fetchActivities()
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Reactivities</h1>
+      <ul>
+        {
+          activities && activities.map((activity: any) => {
+            return (
+              <li key={activity.id}>{activity.title}</li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
